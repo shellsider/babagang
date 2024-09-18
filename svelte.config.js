a48ddc04b86cea710d1087f71e5034ac99ae34a6
shellsider <work.harshdeepsingh@gmail.com>
2024-09-19 01:14:41 +0530

    adaptor digital

--- svelte.config.js
+++ svelte.config.js
@@ -14,35 +14,35 @@
 
 // export default config;
 
-// import adapter from 'svelte-adapter-static-digitalocean';
-// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
-// const config = {
-//     preprocess: vitePreprocess(),
-
-//     kit: {
-//         adapter: adapter({}),
-//     }
-// };
-
-
-// export default config;
-
-import adapter from '@sveltejs/adapter-node';
+import adapter from 'svelte-adapter-static-digitalocean';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
-    preprocess: vitePreprocess(), // or vitePreprocess() depending on what you're using
+    preprocess: vitePreprocess(),
 
     kit: {
-        adapter: adapter({
-            out: 'build', // Optional: Output directory for the built Node.js server
-            precompress: false, // Optional: Enable gzip and brotli compression
-            envPrefix: '' // Optional: Customize environment variable prefixes
-        })
+        adapter: adapter({}),
     }
 };
 
+
 export default config;
+
+// import adapter from '@sveltejs/adapter-node';
+// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+// const config = {
+//     preprocess: vitePreprocess(), // or vitePreprocess() depending on what you're using
+
+//     kit: {
+//         adapter: adapter({
+//             out: 'build', // Optional: Output directory for the built Node.js server
+//             precompress: false, // Optional: Enable gzip and brotli compression
+//             envPrefix: '' // Optional: Customize environment variable prefixes
+//         })
+//     }
+// };
+
+// export default config;
 
 
