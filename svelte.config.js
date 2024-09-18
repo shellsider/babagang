6930f5245600152930846994d069f1cd6af162da
shellsider <work.harshdeepsingh@gmail.com>
2024-09-19 01:00:31 +0530

    configured adaptor

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,15 @@
 import adapter from '@sveltejs/adapter-netlify';
-export default {
-    kit: {
-        // default options are shown
-        adapter: adapter({
-            // if true, will create a Netlify Edge Function rather
-            // than using standard Node-based functions
-            edge: false,
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-            // if true, will split your app into multiple functions
-            // instead of creating a single one for the entire app.
-            // if `edge` is true, this option cannot be used
-            split: false
-        })
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+    // for more information about preprocessors
+    preprocess: vitePreprocess(),
+
+    kit: {
+        adapter: adapter()
     }
 };
+
+export default config;
